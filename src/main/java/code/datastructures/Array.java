421a48963c2c8685f6b17dfb10166cbcda3bfd26
Vineeth <vineeth.madhu@gmail.com>
2020-10-27 23:07:17 -0400

    Added question MoveNegativeElementsToLeft

--- src/main/java/code/datastructures/Array.java
+++ src/main/java/code/datastructures/Array.java
@@ -83,4 +83,14 @@ public class Array {
     }
     return arr;
   }
+
+  public void moveNegativeElementsToLeft(int[] arr) {
+    int j = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] < 0) {
+        swap(arr, i, j);
+        ++j;
+      }
+    }
+  }
 }
--- src/test/java/code/datastructures/ArrayTest.java
+++ src/test/java/code/datastructures/ArrayTest.java
@@ -41,8 +41,13 @@ public class ArrayTest {
 
   @Test
   public void testSortArrayOfZeroOneAndTwo() {
-    int[] temp = {0,0,0,0,1,1,2,2};
-    int[] returnValue = arrayQuestions.sortArrayOfZeroOneAndTwo(new int[] {0,2,1,0,2,0,1,0});
+    int[] temp = {0, 0, 0, 0, 1, 1, 2, 2};
+    int[] returnValue = arrayQuestions.sortArrayOfZeroOneAndTwo(new int[]{0, 2, 1, 0, 2, 0, 1, 0});
     Assert.assertArrayEquals(temp, returnValue);
+  }
+
+  @Test
+  public void testMoveNegativeElementsToLeft() {
+    arrayQuestions.moveNegativeElementsToLeft(TestData.randomNumbers);
   }
 }
