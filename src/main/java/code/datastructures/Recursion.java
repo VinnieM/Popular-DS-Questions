6afd5608cadff0d9e5fc4ffcd0fb7c5ec15aa187
Vineeth <vineeth.madhu@gmail.com>
2020-12-01 17:26:43 -0500

    Find the sum of a number

--- src/main/java/code/datastructures/Recursion.java
+++ src/main/java/code/datastructures/Recursion.java
@@ -11,4 +11,11 @@ public class Recursion {
   public int factorial(int number) {
     return number == 0 ? 1 : number * factorial(number - 1);
   }
+
+  /**
+   * Find the sum of digits of a given number.
+   */
+  public int sumOfDigits(int number) {
+    return number == 0 ? 0 : number + sumOfDigits(number - 1);
+  }
 }
--- src/test/java/code/datastructures/RecursionTest.java
+++ src/test/java/code/datastructures/RecursionTest.java
@@ -12,6 +12,11 @@ public class RecursionTest {
     Assert.assertEquals(recursion.factorial(3), 6);
     Assert.assertEquals(recursion.factorial(0), 1);
     Assert.assertEquals(recursion.factorial(4), 24);
+  }
 
+  @Test
+  public void testSumOfDigits() {
+    Assert.assertEquals(recursion.sumOfDigits(2), 3);
+    Assert.assertEquals(recursion.sumOfDigits(10), 55);
   }
 }
