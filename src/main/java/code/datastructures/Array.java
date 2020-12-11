24bd12323e13ceb3b243bd162b72eadcddfd1ef6
Vineeth <vineeth.madhu@gmail.com>
2020-12-11 10:42:40 -0500

    Removed the ReverseArrayFunction.

--- src/main/java/code/datastructures/Array.java
+++ src/main/java/code/datastructures/Array.java
@@ -8,21 +8,6 @@ import java.util.Stack;
 public class Array {
 
   /**
-   * Reverse an array in place. This is done in O(n) time and O(1) space.
-   */
-  public int[] reverseArray(int[] arr) {
-    int low = 0, high = arr.length - 1;
-    while (low < high) {
-      int temp = arr[high];
-      arr[high] = arr[low];
-      arr[low] = temp;
-      ++low;
-      --high;
-    }
-    return arr;
-  }
-
-  /**
    * Get the Max and Min in an Array in O(n) time and O(1) space.
    */
   public int[] getMaxAndMinElement(int[] arr) {
