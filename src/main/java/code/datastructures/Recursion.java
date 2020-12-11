5bbdee953bf288b32e330e026217c8dc054c0b71
Vineeth <vineeth.madhu@gmail.com>
2020-12-11 11:42:08 -0500

    Removed old functions.

--- src/main/java/code/datastructures/Recursion.java
+++ src/main/java/code/datastructures/Recursion.java
@@ -8,28 +8,6 @@ import java.util.Arrays;
 public class Recursion {
 
   /**
-   * Find the factorial of a number.
-   */
-  public int factorial(int number) {
-    return number == 0 ? 1 : number * factorial(number - 1);
-  }
-
-  /**
-   * Find the sum of digits of a given number.
-   */
-  public int sumOfDigits(int number) {
-    return number == 0 ? 0 : number + sumOfDigits(number - 1);
-  }
-
-  /**
-   * Reverse a string.
-   */
-  public String reverseString(String str) {
-    return str.length() == 0 ? str
-        : str.charAt(str.length() - 1) + reverseString(str.substring(0, str.length() - 1));
-  }
-
-  /**
    * Checks if the String is a palindrome.
    */
   public boolean isPalindrome(String str) {
