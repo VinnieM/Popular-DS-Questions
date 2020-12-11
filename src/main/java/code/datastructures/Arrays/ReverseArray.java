cb2b1cd6628b8054a51e233666af2e1a429639af
Vineeth <vineeth.madhu@gmail.com>
2020-12-11 10:41:40 -0500

    Created ReverseArray Class

---
+++ src/main/java/code/datastructures/Arrays/ReverseArray.java
@@ -0,0 +1,20 @@
+package code.datastructures.Arrays;
+
+public class ReverseArray {
+
+  /**
+   * This function reverses an integer array.
+   * This time complexity is O(n) and space complexity is O(1);
+   */
+  public int[] reverseArray(int[] array) {
+    int low = 0, high = array.length - 1;
+    while (low < high) {
+      int temp = array[low];
+      array[low] = array[high];
+      array[high] = temp;
+      ++low;
+      --high;
+    }
+    return array;
+  }
+}
