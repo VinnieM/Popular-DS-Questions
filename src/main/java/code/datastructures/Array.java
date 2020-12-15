f4c5631aba63f819aeba9c5655e52559f1374ec8
Vineeth <vineeth.madhu@gmail.com>
2020-12-14 22:53:52 -0500

    Updated Arrays Class.

--- src/main/java/code/datastructures/Array.java
+++ src/main/java/code/datastructures/Array.java
@@ -14,27 +14,6 @@ public class Array {
   }
 
   /**
-   * Program to sort an array containing only 0's, 1's and 2 without using any sorting algorithm and
-   * without using any extra memory.
-   */
-  public int[] sortArrayOfZeroOneAndTwo(int[] arr) {
-    int low = 0, mid = 0, high = arr.length - 1;
-    while (mid <= high) {
-      if (arr[mid] == 0) {
-        swap(arr, low, mid);
-        ++low;
-        ++mid;
-      } else if (arr[mid] == 1) {
-        ++mid;
-      } else {
-        swap(arr, mid, high);
-        --high;
-      }
-    }
-    return arr;
-  }
-
-  /**
    * Program to move all the negative elements to the left and positive elements to the right.
    */
   public void moveNegativeElementsToLeft(int[] arr) {
