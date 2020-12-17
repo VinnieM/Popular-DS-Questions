2974c09beadb2d7a61ff24e61e24b562b719368d
Vineeth <vineeth.madhu@gmail.com>
2020-12-17 14:55:34 -0500

    Added Merge Arrays without using extra space.

--- src/main/java/code/datastructures/arrays/ArrayUnion.java
+++ src/main/java/code/datastructures/arrays/ArrayUnion.java
@@ -1,5 +1,7 @@
 package code.datastructures.arrays;
 
+import java.util.Arrays;
+
 public class ArrayUnion {
 
   /**
@@ -27,5 +29,23 @@ public class ArrayUnion {
       arrayToReturn[k++] = array2[j++];
     }
     return arrayToReturn;
+  }
+
+  /**
+   * Merge 2 sorted arrays without using extra space. We need to merge these two arrays such that
+   * the initial numbers (after complete sorting) are in the first array and the remaining numbers
+   * are in the second array.E.G - Array 1 - {3} Array2 {1, 2}. Output should be Array1 - {1} and
+   * Array2 - {2, 3}. Time complexity is O(m log m + n). Space complexity is O(1).
+   * Level - Medium.
+   */
+  public void mergeArrays(int[] array1, int[] array2) {
+    for (int i = 0; i < array1.length - 1; i++) {
+      if (array2[0] > array1[i]) {
+        int temp = array2[0];
+        array2[0] = array1[i + 1];
+        array1[i + 1] = temp;
+        Arrays.sort(array2);
+      }
+    }
   }
 }
