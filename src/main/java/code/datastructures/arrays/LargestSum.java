14dfb072f2e4852387968d45da73185192e62af4
Vineeth <vineeth.madhu@gmail.com>
2020-12-17 21:56:26 -0500

    Added Kadanes Algorithm

--- src/main/java/code/datastructures/arrays/LargestSum.java
+++ src/main/java/code/datastructures/arrays/LargestSum.java
@@ -8,11 +8,25 @@ public class LargestSum {
    * Level - Medium.
    */
   public int largestContiguousSubArray(int[] arr) {
-    int currentMax = arr[0], totalToReturn = 0;
+    int currentMax = arr[0], totalToReturn = arr[0];
     for (int i = 1; i < arr.length; i++) {
       currentMax = Math.max(arr[i], currentMax + arr[i]);
       totalToReturn = Math.max(currentMax, totalToReturn);
     }
     return totalToReturn;
+  }
+
+  /**
+   * This is the Kadane's Algorithm. Help to get the sum of the longest subarray in O(n) time
+   * complexity and O(1) space complexity.
+   * Level - Medium
+   */
+  public int kadanesAlgorithm(int[] arr) {
+    int currentMax = arr[0], max = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+      currentMax = Math.max(arr[i], currentMax + arr[i]);
+      max = Math.max(currentMax, max);
+    }
+    return max;
   }
 }
