6ca3b1389f193c6af8225de48469ffed86a2803e
Vineeth <vineeth.madhu@gmail.com>
2020-12-27 20:06:24 -0500

    Updated Dutch National Flag problem

--- src/main/java/code/datastructures/arrays/SortCustomArray.java
+++ src/main/java/code/datastructures/arrays/SortCustomArray.java
@@ -4,6 +4,7 @@ public class SortCustomArray {
 
   /**
    * Sort an array which contains only 0, 1, 2 in O(n) time and O(1) space.
+   * This algorithm is also called the Dijkstra's Dutch national flag problem.
    */
   public int[] sortCustomArray(int[] arr) {
     int low = 0, mid = 0, high = arr.length - 1;
@@ -26,5 +27,39 @@ public class SortCustomArray {
     int temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
+  }
+
+  /**
+   * Alternate approach to solving the Dutch National problem.
+   */
+  public int[] sortCustomArray_Solution2(int[] arr) {
+    int low = 0, mid = 0, high = 0;
+    for (int i : arr) {
+      if (i == 0) {
+        ++low;
+      } else if (i == 1) {
+        ++mid;
+      } else {
+        ++high;
+      }
+    }
+    int i = 0;
+    while (low > 0) {
+      arr[i] = 0;
+      --low;
+      ++i;
+    }
+    while (mid > 0) {
+      arr[i] = 1;
+      --mid;
+      ++i;
+    }
+    while (high > 0) {
+      arr[i] = 2;
+      --high;
+      ++i;
+    }
+
+    return arr;
   }
 }
