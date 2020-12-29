19d31de7e3244c87fb03897853699c3d3256edba
Vineeth <vineeth.madhu@gmail.com>
2020-12-28 22:36:28 -0500

    Added comments

--- src/main/java/code/datastructures/sort/BubbleSort.java
+++ src/main/java/code/datastructures/sort/BubbleSort.java
@@ -2,6 +2,9 @@ package code.datastructures.sort;
 
 public class BubbleSort {
 
+  /**
+   * This is the standard Bubble sort. The time complexity is O(n2) and Space complexity is O(1).
+   */
   public int[] bubbleSort(int[] arr) {
     for (int i = 0; i < arr.length; i++) {
       for (int j = i; j < arr.length; j++) {
