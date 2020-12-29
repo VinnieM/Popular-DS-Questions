070b991483218673dcd721ddf661712255a711e3
Vineeth <vineeth.madhu@gmail.com>
2020-12-29 00:11:34 -0500

    Added test cases

--- src/test/java/code/datastructures/SortTest.java
+++ src/test/java/code/datastructures/SortTest.java
@@ -1,6 +1,7 @@
 package code.datastructures;
 
 import code.datastructures.sort.BubbleSort;
+import code.datastructures.sort.InsertionSort;
 import code.utils.TestData;
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,5 +13,12 @@ public class SortTest {
     BubbleSort sort = new BubbleSort();
     Assert.assertArrayEquals(sort.bubbleSort(TestData.tinyArrayUnsorted), TestData.tinyArray);
     Assert.assertArrayEquals(sort.bubbleSort(TestData.largeArrayUnsorted), TestData.largeArray);
+  }
+
+  @Test
+  public void testInsertionSort() {
+    InsertionSort sort = new InsertionSort();
+    Assert.assertArrayEquals(sort.insertionSort(TestData.tinyArrayUnsorted), TestData.tinyArray);
+    Assert.assertArrayEquals(sort.insertionSort(TestData.largeArrayUnsorted), TestData.largeArray);
   }
 }
