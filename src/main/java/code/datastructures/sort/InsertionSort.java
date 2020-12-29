9fb39285129a08c5a573a3e7baea9161a620f16c
Vineeth <vineeth.madhu@gmail.com>
2020-12-29 00:11:23 -0500

    Multiple changes
    Added comments and included a return type

--- src/main/java/code/datastructures/sort/InsertionSort.java
+++ src/main/java/code/datastructures/sort/InsertionSort.java
@@ -2,7 +2,10 @@ package code.datastructures.sort;
 
 public class InsertionSort {
 
-  public void insertionSort(int[] arr) {
+  /**
+   * Standard insertion sort algorithm. Time complexity is O(n2) and Space complexity is O(1).
+   */
+  public int[] insertionSort(int[] arr) {
     for (int i = 1; i < arr.length; i++) {
       int value = arr[i], j = i - 1;
       while (j >= 0 && arr[j] > value) {
@@ -11,5 +14,6 @@ public class InsertionSort {
       }
       arr[j + 1] = value;
     }
+    return arr;
   }
 }
