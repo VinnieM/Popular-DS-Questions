e6bbc19c85d7d6ace254123924a3b3f9e5e49e13
Vineeth <vineeth.madhu@gmail.com>
2021-01-02 22:42:04 -0500

    Updated comments

--- src/main/java/code/datastructures/arrays/MoveToLeft.java
+++ src/main/java/code/datastructures/arrays/MoveToLeft.java
@@ -2,9 +2,13 @@ package code.datastructures.arrays;
 
 public class MoveToLeft {
 
+  /**
+   * Move all the zero's to the left while preserving the order of all other elements. Time
+   * complexity is O(n) while space complexity is O(1).
+   */
   public int[] moveZeroToLeft(int[] arr) {
     int j = arr.length - 1;
-    for (int i = arr.length -1 ; i >= 0; i--) {
+    for (int i = arr.length - 1; i >= 0; i--) {
       if (arr[i] != 0) {
         swap(i, j, arr);
         --j;
