f5d82a24de9f7e42fd80feb73dd6aea3f94f8194
Vineeth <vineeth.madhu@gmail.com>
2021-01-02 22:41:54 -0500

    Updated Comments

--- src/main/java/code/datastructures/arrays/MoveElements.java
+++ src/main/java/code/datastructures/arrays/MoveElements.java
@@ -2,6 +2,10 @@ package code.datastructures.arrays;
 
 public class MoveElements {
 
+  /**
+   * Move the specified element to the right side of the array. Time complexity is O(n) and space
+   * complexity is O(1).
+   */
   public int[] moveElementToRight(int[] arr, int elementToMove) {
     int i = 0, j = arr.length - 1;
     while (i < j) {
